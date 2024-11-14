a89f01c5398f3435d5e27e50ca3b7c1ba43684c8
Kirilo Chori <kirilosakanaliev@gmail.com>
2024-11-14 18:23:11 +0200

    Добавьте файлы проекта.

---
+++ Laba 6.3.2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_6._3._2
+{
+    internal class Program
+    {
+            static void Main(string[] args)
+            {
+                int[,] matrix = new int[6, 4];
+
+                FillMatrix(matrix);
+                Console.WriteLine("Generated Matrix:");
+                PrintMatrix(matrix);
+
+                int minColumnIndex;
+                int minColumnSum = FindMinSumColumn(matrix, out minColumnIndex);
+
+                Console.WriteLine($"\nColumn with minimum sum is: {minColumnIndex}");
+                Console.WriteLine($"Minimum sum in the column is: {minColumnSum}");
+            }
+
+          
+            static void FillMatrix(int[,] matrix)
+            {
+                Random rand = new Random();
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        matrix[i, j] = rand.Next(0, 101);
+                    }
+                }
+            }
+
+                 
+            static void PrintMatrix(int[,] matrix)
+            {
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        Console.Write($"{matrix[i, j],4}");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            static int FindMinSumColumn(int[,] matrix, out int minColumnIndex)
+            {
+                int minSum = int.MaxValue;
+                minColumnIndex = -1;
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    int columnSum = 0;
+                    for (int i = 0; i < matrix.GetLength(0); i++)
+                    {
+                        columnSum += matrix[i, j];
+                    }
+
+                    if (columnSum < minSum)
+                    {
+                        minSum = columnSum;
+                        minColumnIndex = j;
+                    }
+                }
+
+                return minSum;
+            }
+        
+    }
+}
+
+                
+           
+
+        
+    
+
+
